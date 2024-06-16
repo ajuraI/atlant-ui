c08b77a87bc37a791d9416a64646945cfb5c308b
ANCHUTIN Yury <YURI.anchutin@raiffeisen.ru>
2024-06-16 20:01:39 +0300

    Исправить стили

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts';
 import macrosPlugin from 'vite-plugin-babel-macros';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts(), macrosPlugin()],
+  plugins: [react(), dts(), macrosPlugin(), libInjectCss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
